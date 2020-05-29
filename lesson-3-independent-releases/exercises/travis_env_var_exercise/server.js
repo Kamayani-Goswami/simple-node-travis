f1e3765cc95b6abf84447d5696ce185b5e7d25bd
Kamayani Goswami <65108045+Kamayani-Goswami@users.noreply.github.com>
2020-05-29 17:34:50 +0530

    Update server.js

--- lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
+++ lesson-3-independent-releases/exercises/travis_env_var_exercise/server.js
@@ -1,15 +1,13 @@
-
+const FAVORITE_INSTRUCTOR = process.env.KESHAWILLIAMS;
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+    console.log(`${FAVORITE_INSTRUCTOR} rocks!`);
     await sleep(5000);
   }
 }
-const favoriteFood = process.env.Pizza;
-console.log(`My favorite food is ${favoriteFood}`);
 
 main();
